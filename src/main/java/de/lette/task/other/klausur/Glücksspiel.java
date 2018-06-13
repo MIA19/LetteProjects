50a9fd2f6512a77fb4db7aa017bcc3b96792b798
zInvalid <rob099@gmx.de>
2018-06-13 14:39:47 +0200

    jdoc

--- src/main/java/de/lette/task/other/klausur/Glücksspiel.java
+++ src/main/java/de/lette/task/other/klausur/Glücksspiel.java
@@ -1,20 +1,33 @@
 package de.lette.task.other.klausur;
 
+/**
+ * With the the class {@link Glücksspiel} you can try you luck in the jackpot, it contains a method to play and a test {@link #main(String[]) main} function
+ */
 public class Glücksspiel {
     private static int jackpot;
     private int einsatz;
 
+    /**
+     * Creates a {@link Glücksspiel} object with the given parameter and adds the bet to the {@link #jackpot}
+     *
+     * @param einsatz the bet you need to place
+     */
     public Glücksspiel(int einsatz) {
         jackpot += einsatz;
         this.einsatz = einsatz;
     }
 
+    /**
+     * You are only able to play one time, your chance to win is 1%
+     *
+     * @throws Exception if you win or play more than one time
+     */
     public void spiele() throws Exception {
         if (einsatz == 0)
             throw new Exception("Das Spiel ist aus");
 
-        int random = (int) (Math.random() * 1000);
-        if (random < 7) {
+        double random = Math.random();
+        if (random < 0.01) {
             if (jackpot != 0) {
                 int jackpot = Glücksspiel.jackpot;
                 Glücksspiel.jackpot = 0;
@@ -28,16 +41,27 @@ public class Glücksspiel {
         einsatz = 0;
     }
 
+    /**
+     * Testing the {@link Glücksspiel}
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         try {
             for (int i = 0; i <= 1000; i++)
-                new Glücksspiel((int) (Math.random() * 90));
+                new Glücksspiel((int) ((Math.random() * 90) + 10)).spiele();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println();
+
+        try {
             Glücksspiel gs = new Glücksspiel(50);
             gs.spiele();
-
             gs.spiele();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/main/java/de/lette/task/other/klausur/HappyException.java
+++ src/main/java/de/lette/task/other/klausur/HappyException.java
@@ -1,11 +1,18 @@
 package de.lette.task.other.klausur;
 
+/**
+ * A exception for the winner of the {@link Glücksspiel#spiele game}
+ */
 public class HappyException extends Exception {
 
     private int gewinn;
 
-    public HappyException(int gewinn)
-    {
+    /**
+     * Creates a {@link HappyException} object with a {@link #gewinn win pool}
+     *
+     * @param gewinn the win pool
+     */
+    public HappyException(int gewinn) {
         super("Dein Gewinn beträgt " + gewinn + "€");
         this.gewinn = gewinn;
     }
